cba93aaccf0ed14808ba8e598cf48904c2380de7
Gangsheng Wang <wang.gangsheng@gmail.com>
2020-06-06 12:51:14 -0500

    1st commit for soil moisture effects on microbial dormancy

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=menddhs
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/menddhs
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=menddhs.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/menddhs.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=menddhs
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/menddhs
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=menddhs.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/menddhs.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
